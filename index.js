3d5d21522bb44a6e8a7e996bd963441fd5f8fd93
sr229 <aguilarralph13@gmail.com>
2017-10-24 17:42:25 +0800

    add basic constructor parameters and added more sutffs

---
+++ index.js
@@ -0,0 +1,4 @@
+// import and export statement polyfill 
+require('import-export');
+
+require('./lib/Wolken');
--- src/Wolken.mjs
+++
@@ -1,6 +0,0 @@
-/**
- * @file A weeb.sh wrapper
- * @author Capuccino
- * @copyright 2017 Clarity
- * @license MIT
- */
