ccb7fcee799ca1e17ba338d08339d0a9c01ef08c
Capuccino <chinodesuuu@gmail.com>
2017-10-25 07:23:58 +0000

    wuck Folke

---
+++ tests/README.md
@@ -0,0 +1,11 @@
+To run the tests you require a file named ``token.json`` with the following data
+
+```json
+{
+   "key": "KEY HERE" 
+}
+```
+After that run by a simple node/<TEST_NAME>. It's that simple.
+
+Testing? I'm done.
+Hotel? Trivago.
---
+++ tests/random.js
@@ -0,0 +1,13 @@
+/**
+ * @file to test Wolken#getRandom
+ * @author Capuccino
+ */
+ 
+ const Wolken = require('../');
+ const token = require('./token.json');
+ const s = new Wolken(token, {
+     //lewd af boi
+     allowHidden: true
+ });
+ 
+ s.getRandom().then(res => console.log(res));
