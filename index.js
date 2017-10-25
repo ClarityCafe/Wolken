b6fc3511e38e17df6070ba85a205d6721441c2c6
sr229 <aguilarralph13@gmail.com>
2017-10-25 18:44:28 +0800

    0.0.2-BETA

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
 // import and export statement polyfill 
 require('import-export');
-
-if (process.version !== 'v8.5.0') require('./lib/Wolken');
-else require('Wolken.Node8');
+require('./lib/Wolken');
